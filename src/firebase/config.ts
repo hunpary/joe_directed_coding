c9107e20ee444cc65204993284210a755aad238a
hunwoo lee <hunpary@gmail.com>
2026-02-19 14:34:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9151406971-7fb2d",
+  "appId": "1:371639435063:web:51ded7c051b6b9a1ed64b2",
+  "apiKey": "AIzaSyD7BamwpZQEs6CMRw_uCzwkJMxnM29Mc1I",
+  "authDomain": "studio-9151406971-7fb2d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "371639435063"
+};
