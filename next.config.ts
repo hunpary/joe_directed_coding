63395e76fa510668914f8212a6bfefb9bf296024
hunwoo lee <hunpary@gmail.com>
2026-02-21 11:41:27 +0000

    remove static export

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // (핵심) 정적 export로 빌드
   typescript: {
     ignoreBuildErrors: true,
   },
